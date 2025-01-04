9ad09233ed20c680182c9ec64edd8dc50bf02718
TavoWEB <bartuseviciusmindaugas@gmail.com>
2025-01-04 17:47:10 +0200

    0.0.14 beta

--- header.php
+++ header.php
@@ -1,53 +1,66 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TavoWEB</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</head>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="TavoWEB">
-    
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/headers/">
-    <link href="css/style.css" rel="stylesheet">
-    <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
-
-  </head>
-  <body>
-  <header class="p-3 bg-dark text-white">
-    
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand logo" href="/"><img class="logo" src="https://www.tavoweb.lt/wp-content/uploads/2020/12/logoj.png"></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a href="pass-generator.php" class="nav-link px-2 text-black">Slaptažodžių generatorius</a>
-            </li>
-            <li class="nav-item">
-                
-            </li>
-            <li class="nav-item">
-                
-            </li>
-            <li class="nav-item dropdown">
-            <li class="nav-item">
-                
-            </li>
-            <li class="nav-item">
-                
-            </li>
-            <li class="nav-item">
-                <a href="https://tavoweb.lt" class="nav-link px-3 text-black">TavoWEB.lt</a>
-            </li>
-  </header>
+<!doctype html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TavoWEB</title>
+    <!-- Bootstrap 5 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- jQuery (reikalingas tik jei naudosite jį su Bootstrap komponentais, pvz., modaliems, kuris nėra būtinas su Bootstrap 5) -->
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
+    <!-- Bootstrap 5 Bundle JS (apima Popper.js ir Bootstrap JS) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="TavoWEB">
+
+
+
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/headers/">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+    <header class="p-3 bg-dark text-white">
+
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand logo" href="/"><img class="logo" src="https://www.tavoweb.lt/wp-content/uploads/2020/12/logoj.png"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a href="pass-generator.php" class="nav-link px-2 text-black">Slaptažodžių generatorius</a>
+                    </li>
+                    <li class="nav-item">
+
+
+                    </li>
+                    <li class="nav-item">
+
+
+                    </li>
+                    <li class="nav-item dropdown">
+                    <li class="nav-item">
+
+
+                    </li>
+                    <li class="nav-item">
+
+
+                    </li>
+                    <li class="nav-item">
+                        <a href="https://tavoweb.lt" class="nav-link px-3 text-black">TavoWEB.lt</a>
+                    </li>
+    </header>
