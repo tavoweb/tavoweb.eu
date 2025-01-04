27163ff94c597b8d4e2502b7a207813029c0f6a7
TavoWEB <bartuseviciusmindaugas@gmail.com>
2025-01-04 15:46:08 +0200

    Update autoupdate.py

--- autoupdate.py
+++ autoupdate.py
@@ -1,19 +1,22 @@
 import os
 import subprocess
 
-# „GitHub“ URL ir vietinė saugyklos vieta
+# „GitHub“ URL ir tikslinis aplankas
 github_url = "https://github.com/tavoweb/tavoweb.eu.git"
 local_repo_path = "/home/admin/domains/tavoweb.eu/public_html"
 
 def update_repo():
     try:
-        # Patikriname, ar aplankas jau egzistuoja
-        if not os.path.exists(local_repo_path):
+        # Tikriname, ar tikslinis aplankas yra Git saugykla
+        if not os.path.exists(target_path) or not os.path.exists(os.path.join(target_path, ".git")):
             print("Klonuojama saugykla...")
-            subprocess.run(["git", "clone", github_url, local_repo_path], check=True)
+            if os.path.exists(target_path):
+                print("Aplankas egzistuoja, bet nėra Git saugykla. Valome aplanką...")
+                subprocess.run(["rm", "-rf", target_path], check=True)
+            subprocess.run(["git", "clone", github_url, target_path], check=True)
         else:
-            # Pereiname į vietinį aplanką ir atnaujiname saugyklą
-            os.chdir(local_repo_path)
+            # Pereiname į tikslinį aplanką ir atnaujiname saugyklą
+            os.chdir(target_path)
             print("Atnaujinama saugykla...")
             subprocess.run(["git", "pull"], check=True)
         print("Veiksmas atliktas sėkmingai!")
@@ -24,3 +27,4 @@ def update_repo():
 
 # Paleidžiame funkciją
 update_repo()
+
